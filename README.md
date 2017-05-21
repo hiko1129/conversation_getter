eb32eebf04ffbe3d4b33d0d4d46758683ae3c543
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2017-05-21 10:22:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+ConversationGetter
+====
+
+ツイッター上での会話を取得します。
+
+## Description
+Twitter APIのstatuses/sampleから会話を取得してDB（sqlite）に保存します。
+
+sequence to sequenceの学習用データとして使えるかと思います。  
+conversationsテーブルにtweetカラムとreplyカラムとして入ります。  
+全ての会話を取得して来る機能はないので、コンテキストが読み取れないようなものが入ります。そのため、学習用データとしてはいまいちかもしれません。  
+
+整形する機能はないので学習前に加工してください。
+
+## Usage
+edit bash profile.  
+export TWITTER_API_KEY='YOUR TWITTER API KEY'  
+export TWITTER_API_SECRET='YOUR TWITTER API SECRET'  
+export TWITTER_ACCESS_TOKEN='YOUR TWITTER ACCESS TOKEN'  
+export TWITTER_ACCESS_TOKEN_SECRET='YOUR TWITTER ACCESS TOKEN SECRET'  
---
+++ conversation_getter.py
@@ -0,0 +1,124 @@
+from twitter import OAuth, OAuth2, oauth2_dance
+from twitter import Twitter, TwitterStream, TwitterHTTPError
+import os
+import sqlite3
+import time
+
+
+class ConversationGetter:
+    """ツイッター上での会話を取得する."""
+
+    def prepare_db(self, db_name):
+        """DBの準備."""
+        if not os.path.exists(db_name):
+            # ファイルが存在しない場合.
+            self.conn = sqlite3.connect(db_name)
+            self.c = self.conn.cursor()
+            query = '''
+            CREATE TABLE tweet_ids (
+                conversation_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                tweet_id TEXT,
+                in_reply_to_status_id TEXT
+            )
+            '''
+            self.c.execute(query)
+            query = '''
+            CREATE TABLE conversations (
+                conversation_id INTEGER primary key,
+                tweet TEXT,
+                reply TEXT,
+                FOREIGN KEY (conversation_id)
+                REFERENCES tweet_ids(conversation_id)
+            )
+            '''
+            self.c.execute(query)
+        else:
+            # ファイルが存在する場合.
+            self.conn = sqlite3.connect(db_name)
+            self.c = self.conn.cursor()
+
+    def fetch_tweet_ids(self):
+        """ツイートに付与されたIDを取得する."""
+        twitter_stream = TwitterStream(auth=self.__setting_oauth())
+        iterator = twitter_stream.statuses.sample(language='ja')
+        for index, msg in enumerate(iterator):
+            print('fetch No.{0}'.format(index + 1))
+            if 'in_reply_to_status_id' not in msg:
+                continue
+            if msg['in_reply_to_status_id']:
+                try:
+                    self.c.execute(
+                        '''
+                        INSERT INTO tweet_ids (tweet_id, in_reply_to_status_id)
+                        VALUES (?, ?)
+                        ''',
+                        (msg['id'], msg['in_reply_to_status_id'])
+                    )
+                    self.conn.commit()
+                except sqlite3.IntegrityError:
+                    continue
+
+    def fetch_conversations(self, limit_num, sleep_minute):
+        """ツイッター上での会話を取得."""
+        self.c.execute('SELECT * FROM tweet_ids')
+        ids = self.c.fetchall()
+        counter = 0
+        for conversation_id, tweet_id, in_reply_to_status_id in ids:
+            counter += 2
+            if counter >= limit_num:
+                time.sleep(sleep_minute * 60)
+                print('--Wait 15 minutes--')
+            print("fetch conversation_id:{0}".format(conversation_id))
+            try:
+                twitter = Twitter(auth=self.__setting_oauth2())
+                tweet = twitter.statuses.show(_id=in_reply_to_status_id)['text']
+                reply = twitter.statuses.show(_id=tweet_id)['text']
+                self.c.execute(
+                    '''
+                    INSERT INTO conversations (conversation_id, tweet, reply)
+                    VALUES (?, ?, ?)
+                    ''',
+                    (conversation_id, tweet, reply)
+                )
+                self.conn.commit()
+            except TwitterHTTPError as err:
+                print('skip this conversation')
+                continue
+            except sqlite3.IntegrityError as err:
+                print('skip this conversation')
+                continue
+        self.conn.close()
+
+    def __setting_oauth(self):
+        """OAuthのための設定を行う."""
+        api_key = os.environ['TWITTER_API_KEY']
+        api_secret = os.environ['TWITTER_API_SECRET']
+        access_token = os.environ['TWITTER_ACCESS_TOKEN']
+        access_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+
+        return OAuth(
+
+            consumer_key=api_key,
+            consumer_secret=api_secret,
+            token=access_token,
+            token_secret=access_secret
+        )
+
+    def __setting_oauth2(self):
+        """OAuth2のための設定を行う."""
+        api_key = os.environ['TWITTER_API_KEY']
+        api_secret = os.environ['TWITTER_API_SECRET']
+
+        return OAuth2(
+            bearer_token=oauth2_dance(
+                api_key,
+                api_secret,
+            )
+        )
+
+
+if __name__ == '__main__':
+    conversation_getter = ConversationGetter()
+    conversation_getter.prepare_db('twitter_conversation.db')
+    conversation_getter.fetch_tweet_ids()
+    # conversation_getter.fetch_conversations(450, 15)
